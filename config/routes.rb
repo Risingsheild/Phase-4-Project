6bea92b77127e04e33572d46d5f387aa01acf9d2
nickwells <wellsnick11@yahoo.com>
2022-09-28 20:22:41 -0700

    Created the necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  resources: reviews
+  resources: platforms, only: [:index, :show]
+  resources: games, only: [:index, :show, :create]
+  resources: users
+
+  get '/me', to: "users#show"
+  post '/login', to: "sessions#login"
+  delete '/logout', to: "sessions#logout"
+
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
