c61d34d05bdc4529a48ddf45b1514c40bd3f2435
nickwells <wellsnick11@yahoo.com>
2022-10-02 15:11:47 -0700

    Login Component Complete

---
+++ client/src/components/Login.js
@@ -0,0 +1,56 @@
+import {useState} from "react"
+
+function Login({setCurrentUser}) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors, setErrors] = useState([])
+    const [login, setLogin] = useState('')
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const user ={
+            username, 
+            password
+        }
+
+    fetch('/users', {
+        method: "POST",
+        headers: {'Content-Type' : 'application/json'},
+        body:JSON.stringify(user)
+    })
+    .then(res => {
+        if (res.ok){
+            res.json().then(setCurrentUser)
+        }
+        else{
+            res.json().then(e => setErrors(Object.entries(e.error).flat())) 
+        }
+    })
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="Login">
+                <label htmlFor="username">Username: </label>
+                <input
+                type="text"
+                id="username" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <input 
+                type="text"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit" onClick={() => setLogin(login)}>Login</button>
+            </form>
+            {errors?errors.map(e => <h3>{e}</h3>):null}
+        </div>
+    )
+}
+
+export default Login
