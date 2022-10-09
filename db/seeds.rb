6f94c371d6761b15b2c0c5bac8835ebb91838910
nickwells <wellsnick11@yahoo.com>
2022-10-09 12:43:56 -0700

    Started Creating Seed Data Add more Games and Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,37 @@
 
 puts "Seeding Data"
 
+user1 = User.create(username: "Nick Wells", password: "123")
+
+#-----PLATFORMS-----
+
+PC = PlatForm.create(
+    title: "PC"
+    image_url: "https://www.pcgamesn.com/wp-content/uploads/2019/02/Best-Gaming-PC.jpg"
+)
+
+XBOX = PlatForm.create(
+    title: "XBOX"
+    image_url: "https://bandwidthblog.co.za/wp-content/uploads/2020/09/xbox-series-x-s-price-south-africa.jpg" 
+)
+
+PlayStation = PlatForm.create(
+    title: "PlayStation"
+    image_url: "https://static.techspot.com/images2/news/bigimage/2020/11/2020-11-06-image-24.jpg"
+)
+
+# ----GAMES----
+LOL = Game.create(
+    title: "League of Legends"
+    genre: "Battle Arena"
+    image_url: "http://appinformers.com/wp-content/uploads/2017/03/league.png"
+)
+
+
+
+
+
+# ----REVIEWS---
 
 
 puts "Seeding done"
